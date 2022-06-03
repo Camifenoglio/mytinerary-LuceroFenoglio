558abb57893aa7b9ba950d0da0c2cfb0f2d6e390
Camila Lucero Fenoglio <cami.fenoglio@gmail.com>
2022-06-03 17:02:20 -0300

    creacion de app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import '../src/styles/App.css';
+import Main from "./components/Main"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>este es el comp app</h1>
+      <Main></Main>
+    </div>
+  );
+}
+
+export default App;
