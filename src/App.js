426464d2f2d3b144d2e9333bd6cedc44a9882482
Camila Lucero Fenoglio <cami.fenoglio@gmail.com>
2022-06-07 10:26:09 -0300

    navbar terminado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import '../src/styles/App.css';
 import Main from "./components/Main"
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>este es el comp app</h1>
+      <NavBar />
+
       <Main></Main>
     </div>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,170 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import "../styles/App.css";
+import Logo from '../assets/logo.png'
+
+const pages = ['Home', 'Cities'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const NavBar = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl" sx={{ backgroundColor: "black" }}>
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            display: { xs: 'none', md: 'flex' },
+                            alignItems: "center",
+                            fontFamily: 'Allura',
+                            fontSize: "2.7rem",
+                            fontWeight: 500,
+                            letterSpacing: '.3rem',
+                            color: 'violet',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        {/* LOGO */}
+                        <img src={Logo} alt="logo" style={{ height: "3.5rem" }} />
+                        MiTinerary
+                    </Typography>
+                    {/* box hamburguesa */}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, color: "violet" }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            alignItems: "center",
+                            flexGrow: 1,
+                            fontFamily: 'Allura',
+                            fontSize: "2.7rem",
+                            fontWeight: 500,
+                            letterSpacing: '.3rem',
+                            color: 'violet',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        {/* LOGO RESPONSIVE */}
+                        <img src={Logo} alt="logo" style={{ height: "3.5rem" }} />
+                        MiTinerary
+                    </Typography>
+                    {/* link paginas home/cities en md */}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 0, color: 'white', display: 'block', fontFamily: 'Vollkorn', fontSize: "1.2rem", mt: 0.5, py: 0 }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                {/* iconologueo */}
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" sx={{ backgroundColor: "#7F19A6", color: "black" }} />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+export default NavBar;
