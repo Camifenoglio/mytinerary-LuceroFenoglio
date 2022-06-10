380cb26933a3fe52578e1746ce1fbb73fa9a9799
Camila Lucero Fenoglio <cami.fenoglio@gmail.com>
2022-06-10 18:46:30 -0300

    cards cities impresas

--- frontend/src/pages/Cities.jsx
+++ frontend/src/pages/Cities.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import '../styles/App.css';
+import cities from '../citiesData/Cities.json';
 
 function Cities() {
     return (
-        <div className='CitiesContainer'>
-        <img src={process.env.PUBLIC_URL+"/assets/underconstruction.png"} alt="underconstruction" style={{height: "60vh"}} />
-        </div>
-    )
+        <>
+        <div className='cardContainer'>
+        {cities.map(citie =>
+        <div key={citie.id}
+        style={{
+          background: `url(${citie.image})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          objectFit: "cover"
+        }}
+        className="card"
+      >
+            <div className="card-info">
+              <h4 className="card-title">{citie.name}</h4>
+              <p className="card-description">{citie.name}</p>
+            </div>
+    </div>
+    )}
+    </div>
+    </>)
+    
 }
 
 export default Cities
